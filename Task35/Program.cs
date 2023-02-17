3bf76e3e767b6832f027d65a7e1eba587562fff5
Vasisualey <basilnekrasov@mail.ru>
2023-02-17 16:11:54 +0300

    решил 35 задачу

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 35. Задайте одномерный массив из 123 случайных чисел.
 // Найдите количество элементов массива, значения которых лежат
 // в отрезке 10- 99.
-
+Console.Clear();
 int[] GetArray(int length, int min, int max)
 {
     int [] res = new int[length];
@@ -23,15 +23,31 @@ int[] GetArray(int length, int min, int max)
     Console.WriteLine();
 }
 
+
+
 int inputnumber(string message)
 {
     Console.Write($"{message}: ");
-    int res = int.Parse(Console.ReadLine());
+    int res = int.Parse(Console.ReadLine()!);
     return res;
 }
 int length = 123;
-int min = inputnumber ("Введите минимальное значение в массиве");
-int max = inputnumber ("Введите максимальное значение в массиве");
-int[] array = GetArray(length);
+int maxinterval = 99;
+int mininterval = 10;
+
+int SummArrayInterval(int[] Somearray,int mininterval, int maxinterval)
+{
+    int sum =0;
+    for(int i =0; i<Somearray.Length; i++)
+    {
+        if (Somearray[i] >= mininterval && Somearray[i]< maxinterval) sum++;
+    }
+    return sum;
+}
+int min = inputnumber("Введите минимальное значение в массиве");
+int max = inputnumber("Введите максимальное значение в массиве");
+int[] array = GetArray(length,min,max);
+int sum = SummArrayInterval(array, mininterval, maxinterval);
 
 Print(array);
+Console.WriteLine($"Количество элементов в заданном отрезке = {sum}");
