df27a286eb007527f892299300572317964a2e0c
*Vasisualey* <basilnekrasov@mail.ru>
2023-02-19 12:57:26 +0300

    решил 34 задачу

--- Domashka/Task34/Program.cs
+++ Domashka/Task34/Program.cs
@@ -3,7 +3,17 @@
 
 //[345, 897, 568, 234] -> 2
 
-// методы
+// 
+int length = inputnumber("Введите количество элементов в массиве");
+int min = 100;
+int max = 1000;
+int inputnumber(string message)
+{
+    Console.Write($"{message}: ");
+    int res = int.Parse(Console.ReadLine()!);
+    return res;
+}
+
 int[] GetArray(int length, int min, int max)
 {
     int [] res = new int[length];
@@ -13,5 +23,28 @@ int[] GetArray(int length, int min, int max)
     }
     return res;
 }
+void Print(int[] Somearray)
+{
+    Console.Write("[");
+    for(int i = 0; i<Somearray.Length-1;i++)
+    {
+        Console.Write($"{Somearray[i]}, ");
+    }
+        Console.Write($"{Somearray[Somearray.Length-1]}");
+    Console.Write("]");
+    Console.WriteLine();
+}
+int[] array = GetArray(length,min,max);
+Print(array);
 
-
+int quantity(int[] Somearray)
+{
+    int sum =0;
+    for(int i =0; i<Somearray.Length; i++)
+    {
+        if (Somearray[i]%2 == 0) sum++;
+    }
+    return sum;
+}
+int sum = quantity(array);
+Console.WriteLine($"Количество четных элементов в массиве = {sum}");
